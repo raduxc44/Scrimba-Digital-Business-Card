76c44947fbf91d85d5c2c1fd0d35c4ed419add1a
raduxc44 <raduxc44@gmail.com>
2022-07-03 00:20:09 +0300

    Finished the layout

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,11 @@
 function Footer() {
     return(
-        <h1>Footer</h1>
+        <div className="footer-container">
+            <i class="fa-brands fa-twitter-square fa-xl"></i>
+            <i class="fa-brands fa-facebook-square fa-xl"></i>
+            <i class="fa-brands fa-instagram-square fa-xl"></i>
+            <i class="fa-brands fa-github-square fa-xl"></i>
+        </div>
     )
 }
 
